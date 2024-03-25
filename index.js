b2ef81ae1a32b4b99d30329dfa4f3f44939b386c
Ivan <cuadradoivan1990@gmail.com>
2024-03-25 19:48:56 -0300

    Add AWS SDK, file upload functionality, and compress-upload endpoint

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
 const express = require('express');
 const sharp = require('sharp');
 const bodyParser = require('body-parser');
-
+const AWS = require('aws-sdk');
+const fileUpload = require('express-fileupload')
+const {  S3Client, PutObjectCommand } = require("@aws-sdk/client-s3");
+require('dotenv').config()
+const { v4: uuidv4 } = require('uuid');
 const app = express();
+
+const client = new S3Client({
+  region: process.env.AWS_REGION,
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID || '',
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY || ''
+  }
+});
+
 
 // Increase payload size limit (e.g., 100MB)
 app.use(bodyParser.json({ limit: '100mb' }));
 app.use(bodyParser.urlencoded({ limit: '100mb', extended: true }));
-
+app.use(fileUpload())
 
 const port = 8000;
 // Enable CORS
@@ -20,14 +33,28 @@ app.use((req, res, next) => {
 
 app.use(express.json());
 
+app.post('/compress-upload', async (req, res) => {
+  
+  let images = req.files.images;
+  if (!Array.isArray(images)) {
+    images = [images];
+  }
+  const urls = [];
+  for (const image of images) {
+    const newBUffer = await compressImage(image.data, 100 )//targetSizeKB)
+    image.data = newBUffer;
+    const u = await uploadToS3(image);
+    urls.push(u);
+  }
+  console.log('Urls', urls);
+  return res.status(200).send({ message: 'File upload', urls: urls });
+});
+// })
+
 app.post('/compress-image', async (req, res) => {
   try {
-    const { imageBuffer, targetSizeKB } = req.body;
-    
-    // Compress the image using your compressImage function
-    const compressedImageBuffer = await compressImage(imageBuffer, targetSizeKB);
-    
-    // Send the compressed image back as a response
+    const { imageBuffer, targetSizeKB, images } = req.body;
+    const compressedImageBuffer = await compressImage(imageBuffer, targetSizeKB);     
     res.send({ compressedImageBuffer });
   } catch (error) {
     console.error('Error compressing image:', error);
@@ -36,6 +63,7 @@ app.post('/compress-image', async (req, res) => {
 });
 
 async function compressImage(imageBuffer, targetSizeKB) {
+  // console.log('Received image buffer:', imageBuffer, 'bytes')
   let compressedImageBuffer = imageBuffer;
   let currentQuality = 80; // Initial quality setting
 
@@ -47,7 +75,6 @@ async function compressImage(imageBuffer, targetSizeKB) {
         .jpeg({ quality: currentQuality, mozjpeg: true, chromaSubsampling: '4:4:4' })
         .toBuffer();
 
-      // Decrease quality for next iteration
       currentQuality -= 10;
     } catch (error) {
       console.error('Error compressing image:', error);
@@ -58,6 +85,45 @@ async function compressImage(imageBuffer, targetSizeKB) {
   return compressedImageBuffer;
 }
 
+async function uploadToS3(image) {
+  const extension = image.mimetype.split('/')[1];
+  const key = `images/${uuidv4()}.${extension}`;
+  const uploadParams = {
+    Bucket: process.env.AWS_BUCKET_NAME,
+    Key: key, // `images/${uuidv4()}.${extension}`,
+    Body: image.data,
+    ContentType: image.mimetype,
+  };
+
+  try {
+    const data = await client.send(new PutObjectCommand(uploadParams));
+    console.log('Image uploaded to S3:', data);
+    // return data.Location;
+    return `https://${process.env.AWS_BUCKET_NAME}.s3.amazonaws.com/${key}`;
+  } catch (error) {
+    console.error('Error uploading image to S3:', error);
+    throw error;
+  }
+}
+
+// 
+
+async function flipAndWatermarkImage(fileBuffer, watermarkPath) {
+  // Load the watermark image
+  const watermarkBuffer = await sharp(watermarkPath)
+      .resize(200) // Resize watermark
+      .toBuffer();
+
+  return sharp(fileBuffer)
+      .flip() // Flip the image horizontally
+      .composite([{ input: watermarkBuffer, gravity: 'southeast' }]) // Add watermark
+      .toBuffer();
+}
+
+// const processedBuffer = await flipAndWatermarkImage(file.buffer, path.join(__dirname, 'logo.png'))
+
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
+
+
